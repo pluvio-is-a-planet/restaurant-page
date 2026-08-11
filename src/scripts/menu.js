3b7ad6d70123a6c6210fa0c85dd29735d835926f
pluvio.isaplanet <308019814+pluvio-is-a-planet@users.noreply.github.com>
2026-08-12 00:12:57 +0200

    Move menu images to own folder

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -4,16 +4,16 @@ import "../assets/stylesheets/menu.css";
 
 import menuData from "../menuData.json";
 
-import blackBriarReserve from "../assets/images/black-briar-reserve.png";
-import bread from "../assets/images/bread.png";
-import grilledLeeks from "../assets/images/grilled-leeks.png";
-import honningbrewMead from "../assets/images/honningbrew-mead.png";
-import horkerStew from "../assets/images/horker-stew.png";
-import nordsAle from "../assets/images/nords-ale.png";
-import salmonSteak from "../assets/images/salmon-steak.png";
-import spicedWine from "../assets/images/spiced-wine.png";
-import sweetroll from "../assets/images/sweetroll.png";
-import venisonStew from "../assets/images/venison-stew.png";
+import blackBriarReserve from "../assets/images/menu/black-briar-reserve.png";
+import bread from "../assets/images/menu/bread.png";
+import grilledLeeks from "../assets/images/menu/grilled-leeks.png";
+import honningbrewMead from "../assets/images/menu/honningbrew-mead.png";
+import horkerStew from "../assets/images/menu/horker-stew.png";
+import nordsAle from "../assets/images/menu/nords-ale.png";
+import salmonSteak from "../assets/images/menu/salmon-steak.png";
+import spicedWine from "../assets/images/menu/spiced-wine.png";
+import sweetroll from "../assets/images/menu/sweetroll.png";
+import venisonStew from "../assets/images/menu/venison-stew.png";
 
 const imageMap = { blackBriarReserve, bread, grilledLeeks, honningbrewMead, horkerStew, nordsAle, salmonSteak, spicedWine, sweetroll, venisonStew };
 
