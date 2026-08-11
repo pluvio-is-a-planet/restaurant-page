b15fedd0b7962f4604323516e670bd133408a19a
pluvio.isaplanet <308019814+pluvio-is-a-planet@users.noreply.github.com>
2026-08-11 23:17:36 +0200

    Add click handlers to switch pages via nav buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import backgroundImage from "./assets/images/bannered-mare-interior.jpg";
 import buildHome from "./scripts/home.js";
+import buildMenu from "./scripts/menu.js";
+import buildAbout from "./scripts/about.js";
 
-buildHome();
+import "./assets/stylesheets/index.css";
+
+(() => {
+    const pageBuilders = {
+        home: buildHome,
+        menu: buildMenu,
+        about: buildAbout
+    };
+
+    function capitalizeFirst(string) {
+        return String(string).charAt(0).toUpperCase() + String(string).slice(1);
+    }
+
+    let activeBtn = document.querySelector(".nav-btn.active");
+
+    const navButtons = document.querySelectorAll(".nav-btn");
+    navButtons.forEach(btn => {
+        btn.addEventListener("click", e => {
+
+            activeBtn?.classList.remove("active");
+            btn.classList.add("active");
+            activeBtn = btn;
+            
+            pageBuilders[btn.dataset.id]();
+        });
+    });
+
+    buildHome();
+})();
