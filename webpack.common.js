f6c8d10298016ee04cc7a635c7a990148d3efd48
pluvio.isaplanet <308019814+pluvio-is-a-planet@users.noreply.github.com>
2026-08-12 12:29:46 +0200

    Split webpack config into common, dev, and prod files

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,15 @@
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-	mode: "development",
-	entry: "./src/index.js",
+	entry: {
+        app: "./src/index.js",
+    },
 	output: {
 		filename: "main.js",
 		path: path.resolve(import.meta.dirname, "dist"),
 		clean: true,
-	},
-	devtool: "eval-source-map",
-	devServer: {
-		watchFiles: ["./src/template.html"],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
